0d0ca99ee0f12f185dbb39a199d26c793bb63069
mahmadzadeh <mahmadzadeh@gmail.com>
2017-10-10 21:02:59 -0700

    Small refactoring

--- app/src/main/java/com/dualnback/NBackVersion.java
+++ app/src/main/java/com/dualnback/NBackVersion.java
@@ -3,18 +3,15 @@ package com.dualnback;
 
 enum NBackVersion {
 
-    OneBack(1),
-    TwoBack(2),
-    ThreeBack(3),
-    FourBack(4),
-    FiveBack(5);
-
+    OneBack( 1 ),
+    TwoBack( 2 ),
+    ThreeBack( 3 ),
+    FourBack( 4 ),
+    FiveBack( 5 );
 
     int howFarBack;
 
     NBackVersion( int i ) {
-        howFarBack  = i;
+        howFarBack = i;
     }
-
-
 }
--- app/src/main/java/com/dualnback/UserInputEvaluation.java
+++ app/src/main/java/com/dualnback/UserInputEvaluation.java
@@ -1,13 +1,8 @@
 package com.dualnback;
 
-/**
- * Created by mahmadzadeh on 04/10/17.
- */
-
 enum UserInputEvaluation {
     CorrectSoundAndLocation,
     IncorrectSoundAndLocation,
     CorrectSoundIncorrectLocation,
     IncorrectSoundCorrectLocation;
-
 }
